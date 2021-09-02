cc338ae6c1e7353c2029f17cc336162df0eda4fe
segandiaye <segadembandiaye2007@gmail.com>
2021-09-02 11:38:14 +0200

    translate fr to en

--- JAVA8/CHALLENGES/FLATMAP-REVERSING/java/TestUnitRunner.java
+++ JAVA8/CHALLENGES/FLATMAP-REVERSING/java/TestUnitRunner.java
@@ -6,12 +6,12 @@ public class TestUnitRunner {
    public static void main(String[] args) {
       Result result = JUnitCore.runClasses(JUnitTests.class);
 
-      System.out.printf("Test exécuté: %s, Échoué: %s%n", result.getRunCount(), result.getFailureCount());
+      System.out.printf("Test run: %s, Fail: %s%n", result.getRunCount(), result.getFailureCount());
 
       for (Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
       }
 
-      System.out.println("Est-ce que tout est OK? : " + result.wasSuccessful());
+      System.out.println("Is everything OK? : " + result.wasSuccessful());
    }
 }
